77022ef58401c0aa9fada97fbcd4fb5ff3e6551c
Phuc Le <phuc.le-thanh@banvien.com.vn>
2024-01-25 11:44:02 +0700

    [BUG-2] Use array instead of doubly linked list to count letters' frequency Phenomenon/Root cause: - To build Huffman Tree, a min-heap must be created - Doubly linked list uses left and right attributes to store the previous and next nodes
    Solution:
    - Use array list instead of doubly linked list when building Huffman Tree
    
    Tested by:
    - Tested passed in step 1.1 and 1.2 (command LAPSE)
    
    Signed-off-by: Phuc Le <phuc.le-thanh@banvien.com.vn>

--- restaurant.cpp
+++ restaurant.cpp
@@ -21,95 +21,87 @@ struct HuffNode_T* newHuffNode(char letter, int freq, struct HuffNode_T *left, s
 /*------------- CODE END: Define a node in Huffman Tree -------------*/
 void LAPSE(string name) {
 	/* Count frequency of letters */
-	struct HuffNode_T* list_node = newHuffNode(name[0], 1, NULL, NULL);
-	struct HuffNode_T* tail_of_list = list_node;
-	for (int i = 1; i < name.length(); i++) {
-		struct HuffNode_T* tmp = list_node;
-		while (tmp != NULL) {
-			if (name[i] == tmp->letter) {
-				tmp->freq++;
-				break;
-			}
-			tmp = tmp->right;
+	struct HuffNode_T* char_arr = (struct HuffNode_T*) malloc(sizeof(struct HuffNode_T) * name.length());
+	int char_arr_size = 0;
+	for (int i = 0; i < name.length(); i++) {
+		int j = 0;
+		for (j = 0; j < char_arr_size; j++) {
+			if (name[i] == char_arr[j].letter) break;
 		}
-		if (tmp == NULL) {
-			tail_of_list->right = newHuffNode(name[i], 1, NULL, NULL);
-			tail_of_list = tail_of_list->right;
+		if (j == char_arr_size) {
+			char_arr[char_arr_size].letter = name[i];
+			char_arr[char_arr_size].freq = 1;
+			char_arr_size++;
+		} else {
+			char_arr[j].freq++;
 		}
 	}
+	char_arr = (struct HuffNode_T*) realloc(char_arr, sizeof(struct HuffNode_T) * char_arr_size);
 	// Unit Test
-	struct HuffNode_T* tmp = list_node;
-	while (tmp != NULL) {
-		cout << tmp->letter << " - " << tmp->freq << endl;
-		tmp = tmp->right;
+	for (int i = 0; i < char_arr_size; i++) {
+		cout << char_arr[i].letter << " - " << char_arr[i].freq << endl;
 	}
 	/* Apply Caeser cipher */
 	for (int i = 0; i < name.length(); i++) {
-		struct HuffNode_T* tmp = list_node;
-		while (tmp != NULL) {
-			if (tmp->letter == name[i]) break;
-			tmp = tmp->right;
+		int j = 0;
+		for (j = 0; j < char_arr_size; j++) {
+			if (char_arr[j].letter == name[i]) break;
 		}
-		if (name[i] >= 'a' && name[i] <= 'z') name[i] = (name[i] - 'a' + tmp->freq) % 26 + 'a';
-		else name[i] = (name[i] - 'A' + tmp->freq) % 26 + 'A';
+		if (name[i] >= 'a' && name[i] <= 'z') name[i] = (name[i] - 'a' + char_arr[j].freq) % 26 + 'a';
+		else name[i] = (name[i] - 'A' + char_arr[j].freq) % 26 + 'A';
 	}
 	/* Clear raw list */
-	while (list_node != NULL) {
-		struct HuffNode_T* prev = list_node;
-		list_node = list_node->right;
-		delete prev;
-	}
-	tail_of_list = NULL;
+	delete char_arr;
 	cout << "New name: " << name << endl;
-	/* Count frequency of letters */
-	list_node = newHuffNode(name[0], 1, NULL, NULL);
-	tail_of_list = list_node;
-	for (int i = 0; i < name.length(); i++) {
-		struct HuffNode_T* tmp = list_node;
-		while (tmp != NULL) {
-			if (name[i] == tmp->letter) break;
-			tmp = tmp->right;
-		}
-		if (tmp == NULL || i == 0) {
-			int count = 0;
-			for (int j = i; j < name.length(); j++) {
-				if (name[i] == name[j]) count++;
-			}
-			if (i == 0) list_node->freq = count;
-			else {
-				tmp = list_node;
-				while (tmp != NULL) {
-					if (count < tmp->freq) break;
-					if (count == tmp->freq) {
-						if ((name[i] >= 'a' && name[i] <= 'z') && (tmp->letter >= 'A' && tmp->letter <= 'Z')) break;
-						if ((name[i] >= 'a' && name[i] <= 'z') && (tmp->letter >= 'a' && tmp->letter <= 'z') && name[i] < tmp->letter) break;
-						if ((name[i] >= 'A' && name[i] <= 'Z') && (tmp->letter >= 'A' && tmp->letter <= 'Z') && name[i] < tmp->letter) break;
-					}
-					tmp = tmp->right;
-				}
-				if (tmp != NULL) {
-					struct HuffNode_T* prev = tmp->left;
-					if (prev == NULL) {
-						prev = newHuffNode(name[i], count, NULL, list_node);
-						tmp->left = prev;
-						list_node = prev;
-					} else {
-						prev->right = newHuffNode(name[i], count, prev, tmp);
-						tmp->left = prev->right;
-					}
-				} else {
-					tail_of_list->right = newHuffNode(name[i], count, tail_of_list, NULL);
-					tail_of_list = tail_of_list->right;
-				}
-			}
-		}
-	}
-	// Unit Test
-	tmp = list_node;
-	while (tmp != NULL) {
-		cout << tmp->letter << " - " << tmp->freq << endl;
-		tmp = tmp->right;
-	}
+	// /* Count frequency of letters */
+	// list_node = newHuffNode(name[0], 1, NULL, NULL);
+	// tail_of_list = list_node;
+	// for (int i = 0; i < name.length(); i++) {
+	// 	struct HuffNode_T* tmp = list_node;
+	// 	while (tmp != NULL) {
+	// 		if (name[i] == tmp->letter) break;
+	// 		tmp = tmp->right;
+	// 	}
+	// 	if (tmp == NULL || i == 0) {
+	// 		int count = 0;
+	// 		for (int j = i; j < name.length(); j++) {
+	// 			if (name[i] == name[j]) count++;
+	// 		}
+	// 		if (i == 0) list_node->freq = count;
+	// 		else {
+	// 			tmp = list_node;
+	// 			while (tmp != NULL) {
+	// 				if (count < tmp->freq) break;
+	// 				if (count == tmp->freq) {
+	// 					if ((name[i] >= 'a' && name[i] <= 'z') && (tmp->letter >= 'A' && tmp->letter <= 'Z')) break;
+	// 					if ((name[i] >= 'a' && name[i] <= 'z') && (tmp->letter >= 'a' && tmp->letter <= 'z') && name[i] < tmp->letter) break;
+	// 					if ((name[i] >= 'A' && name[i] <= 'Z') && (tmp->letter >= 'A' && tmp->letter <= 'Z') && name[i] < tmp->letter) break;
+	// 				}
+	// 				tmp = tmp->right;
+	// 			}
+	// 			if (tmp != NULL) {
+	// 				struct HuffNode_T* prev = tmp->left;
+	// 				if (prev == NULL) {
+	// 					prev = newHuffNode(name[i], count, NULL, list_node);
+	// 					tmp->left = prev;
+	// 					list_node = prev;
+	// 				} else {
+	// 					prev->right = newHuffNode(name[i], count, prev, tmp);
+	// 					tmp->left = prev->right;
+	// 				}
+	// 			} else {
+	// 				tail_of_list->right = newHuffNode(name[i], count, tail_of_list, NULL);
+	// 				tail_of_list = tail_of_list->right;
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// // Unit Test
+	// tmp = list_node;
+	// while (tmp != NULL) {
+	// 	cout << tmp->letter << " - " << tmp->freq << endl;
+	// 	tmp = tmp->right;
+	// }
 }
 
 void KOKUSEN() {
