599880cd84c89fc784504db9c1cb08baa78dd232
Le Thanh Phuc <phuc.le-thanh@internal.banvien.com>
2024-01-31 13:50:22 +0700

    [BUG-5] Restructure heap for Huffman Phenomenon: - Cannot free heap after building Huffman tree absolutely
    Root cause:
    - Pointers in heap change after building Huffman tree
    
    Solution:
    - Back to the previous structure (before changing in BUG-3): keep using
      heap with elements which are letter/merged node
    - Change relevant methods to adapt this structure
    
    Tested by:
    - Check memory leak by valgrind tool
    
    Signed-off-by: Le Thanh Phuc <phuc.le-thanh@internal.banvien.com>

--- restaurant.cpp
+++ restaurant.cpp
@@ -19,46 +19,44 @@ struct HuffNode_T* newHuffNode(unsigned char letter, int freq, struct HuffNode_T
 	return new_node;
 }
 
-int countFreqOfLetter(string name, struct HuffNode_T*** char_arr_addr) {
-	struct HuffNode_T** char_arr = *char_arr_addr;
-	char_arr = (struct HuffNode_T**) malloc(sizeof(struct HuffNode_T*) * name.length());
-	for (int i = 0; i < name.length(); i++) char_arr[i] = NULL;
+int countFreqOfLetter(string name, struct HuffNode_T** char_arr_addr) {
+	struct HuffNode_T* char_arr = *char_arr_addr;
+	char_arr = (struct HuffNode_T*) malloc(sizeof(struct HuffNode_T) * name.length());
 	int char_arr_size = 0;
 	for (int i = 0; i < name.length(); i++) {
 		int j = 0;
-		while (char_arr[j] != NULL) {
-			if (name[i] == char_arr[j]->letter) break;
-			j++;
+		for (j = 0; j < char_arr_size; j++) {
+			if (name[i] == char_arr[j].letter) break;
 		}
 		if (j == char_arr_size) {
-			char_arr[char_arr_size] = newHuffNode(name[i], 1, NULL, NULL);
+			char_arr[char_arr_size].letter = name[i];
+			char_arr[char_arr_size].freq = 1;
+			char_arr[char_arr_size].left = NULL;
+			char_arr[char_arr_size].right = NULL;
 			char_arr_size++;
 		} else {
-			char_arr[j]->freq++;
+			char_arr[j].freq++;
 		}
 	}
-	char_arr = (struct HuffNode_T**) realloc(char_arr, sizeof(struct HuffNode_T*) * char_arr_size);
+	char_arr = (struct HuffNode_T*) realloc(char_arr, sizeof(struct HuffNode_T) * char_arr_size);
 	*char_arr_addr = char_arr;
 	return char_arr_size;
 }
 
 string applyCaesarCipher(string name) {
 	/* Count frequency of letters */
-	struct HuffNode_T** char_arr = NULL;
+	struct HuffNode_T* char_arr = NULL;
 	int char_arr_size = countFreqOfLetter(name, &char_arr);
 	/* Apply Caeser cipher */
 	for (int i = 0; i < name.length(); i++) {
 		int j = 0;
 		for (j = 0; j < char_arr_size; j++) {
-			if (char_arr[j]->letter == name[i]) break;
+			if (char_arr[j].letter == name[i]) break;
 		}
-		if (name[i] >= 'a' && name[i] <= 'z') name[i] = (name[i] - 'a' + char_arr[j]->freq) % 26 + 'a';
-		else name[i] = (name[i] - 'A' + char_arr[j]->freq) % 26 + 'A';
+		if (name[i] >= 'a' && name[i] <= 'z') name[i] = (name[i] - 'a' + char_arr[j].freq) % 26 + 'a';
+		else name[i] = (name[i] - 'A' + char_arr[j].freq) % 26 + 'A';
 	}
 	/* Clear raw list */
-	for (int i = 0; i < char_arr_size; i++) {
-		delete char_arr[i];
-	}
 	delete char_arr;
 	return name;
 }
@@ -71,35 +69,35 @@ bool isLowerCase(unsigned char letter) {
 	return (letter >= 'a' && letter <= 'z');
 }
 
-bool isASmallerThanB(struct HuffNode_T* node_A, struct HuffNode_T* node_B) {
-	if (node_A->freq < node_B->freq) return true;
-	if (node_A->freq == node_B->freq) {
-		if ((isUpperCase(node_A->letter) || isLowerCase(node_A->letter)) && 
-			(isUpperCase(node_B->letter) || isLowerCase(node_B->letter))) {
+bool isASmallerThanB(struct HuffNode_T node_A, struct HuffNode_T node_B) {
+	if (node_A.freq < node_B.freq) return true;
+	if (node_A.freq == node_B.freq) {
+		if ((isUpperCase(node_A.letter) || isLowerCase(node_A.letter)) && 
+			(isUpperCase(node_B.letter) || isLowerCase(node_B.letter))) {
 			// Both A and B are letter nodes
-			return ((isLowerCase(node_A->letter) && isUpperCase(node_B->letter)) || 
-					(isLowerCase(node_A->letter) && isLowerCase(node_B->letter) && node_A->letter < node_B->letter) ||
-					(isUpperCase(node_A->letter) && isUpperCase(node_B->letter) && node_A->letter < node_B->letter));
-		} else if (isUpperCase(node_A->letter) || isLowerCase(node_A->letter)) {
+			return ((isLowerCase(node_A.letter) && isUpperCase(node_B.letter)) || 
+					(isLowerCase(node_A.letter) && isLowerCase(node_B.letter) && node_A.letter < node_B.letter) ||
+					(isUpperCase(node_A.letter) && isUpperCase(node_B.letter) && node_A.letter < node_B.letter));
+		} else if (isUpperCase(node_A.letter) || isLowerCase(node_A.letter)) {
 			// A is letter node, B is merged node
 			return true;
-		} else if (isUpperCase(node_B->letter) || isLowerCase(node_B->letter)) {
+		} else if (isUpperCase(node_B.letter) || isLowerCase(node_B.letter)) {
 			// A is merged node, B is letter node
 			return false;
 		} else {
 			// Both A and B are merged nodes
-			if (node_A->letter < node_B->letter) return true;
+			if (node_A.letter < node_B.letter) return true;
 		}
 	}
 	return false;
 }
 
-void reheapUp(struct HuffNode_T*** min_heap_addr, int pos) {
-	struct HuffNode_T** min_heap = *min_heap_addr;
+void reheapUp(struct HuffNode_T** min_heap_addr, int pos) {
+	struct HuffNode_T* min_heap = *min_heap_addr;
 	if (pos > 0) {
 		int parent = (pos - 1) / 2;
 		if (isASmallerThanB(min_heap[pos], min_heap[parent])) {
-			struct HuffNode_T* tmp_data = min_heap[parent];
+			struct HuffNode_T tmp_data = min_heap[parent];
 			min_heap[parent] = min_heap[pos];
 			min_heap[pos] = tmp_data;
 			reheapUp(&min_heap, parent);
@@ -108,8 +106,8 @@ void reheapUp(struct HuffNode_T*** min_heap_addr, int pos) {
 	*min_heap_addr = min_heap;
 }
 
-void reheapDown(struct HuffNode_T*** min_heap_addr, int pos, int last_pos) {
-	struct HuffNode_T** min_heap = *min_heap_addr;
+void reheapDown(struct HuffNode_T** min_heap_addr, int pos, int last_pos) {
+	struct HuffNode_T* min_heap = *min_heap_addr;
 	int left = pos * 2 + 1;
 	int right = pos * 2 + 2;
 	if (left <= last_pos) {
@@ -118,7 +116,7 @@ void reheapDown(struct HuffNode_T*** min_heap_addr, int pos, int last_pos) {
 			smaller = right;
 		}
 		if (isASmallerThanB(min_heap[smaller], min_heap[pos])) {
-			struct HuffNode_T* tmp_data = min_heap[smaller];
+			struct HuffNode_T tmp_data = min_heap[smaller];
 			min_heap[smaller] = min_heap[pos];
 			min_heap[pos] = tmp_data;
 			reheapDown(&min_heap, smaller, last_pos);
@@ -127,8 +125,8 @@ void reheapDown(struct HuffNode_T*** min_heap_addr, int pos, int last_pos) {
 	*min_heap_addr = min_heap;
 }
 
-void buildHeap(struct HuffNode_T*** char_arr_addr, int size) {
-	struct HuffNode_T** char_arr = *char_arr_addr;
+void buildHeap(struct HuffNode_T** char_arr_addr, int size) {
+	struct HuffNode_T* char_arr = *char_arr_addr;
 	int last_pos = size - 1;
 	int pos = (last_pos - 1) / 2;
 	for (int i = pos; i >= 0; i--) {
@@ -137,10 +135,11 @@ void buildHeap(struct HuffNode_T*** char_arr_addr, int size) {
 	*char_arr_addr = char_arr;
 }
 
-struct HuffNode_T* popHeap(struct HuffNode_T*** min_heap_addr, int* size) {
+struct HuffNode_T* popHeap(struct HuffNode_T** min_heap_addr, int* size) {
 	if (*size == 0) return NULL;
-	struct HuffNode_T** min_heap = *min_heap_addr;
-	struct HuffNode_T* ret = min_heap[0];
+	struct HuffNode_T* min_heap = *min_heap_addr;
+	struct HuffNode_T* ret = (struct HuffNode_T*) malloc(sizeof(struct HuffNode_T));
+	memcpy(ret, &min_heap[0], sizeof(struct HuffNode_T));
 	(*size)--;
 	min_heap[0] = min_heap[*size];
 	reheapDown(&min_heap, 0, (*size) - 1);
@@ -148,8 +147,8 @@ struct HuffNode_T* popHeap(struct HuffNode_T*** min_heap_addr, int* size) {
 	return ret;
 }
 
-void pushHeap(struct HuffNode_T* merged_node, struct HuffNode_T*** min_heap_addr, int* size) {
-	struct HuffNode_T** min_heap = *min_heap_addr;
+void pushHeap(struct HuffNode_T merged_node, struct HuffNode_T** min_heap_addr, int* size) {
+	struct HuffNode_T* min_heap = *min_heap_addr;
 	min_heap[*size] = merged_node;
 	reheapUp(&min_heap, *size);
 	(*size)++;
@@ -163,31 +162,52 @@ unsigned char nextOrderNumber(unsigned char cur_order_num) {
 	return cur_order_num;
 }
 
-void buildHuff(struct HuffNode_T*** tree_heap_addr, int heap_size) {
-	struct HuffNode_T** tree_heap = *tree_heap_addr;
-	struct HuffNode_T *tmp1, *tmp2, *tmp3;
+void buildHuff(struct HuffNode_T** tree_heap_addr, int heap_size) {
+	struct HuffNode_T* tree_heap = *tree_heap_addr;
+	struct HuffNode_T *tmp1, *tmp2, tmp3;
 	unsigned char order_num = 0;
 	while (heap_size > 1) {
 		tmp1 = popHeap(&tree_heap, &heap_size);
 		tmp2 = popHeap(&tree_heap, &heap_size);
-		tmp3 = newHuffNode(order_num, (tmp1->freq) + (tmp2->freq), tmp1, tmp2);
+		tmp3.letter = order_num;
+		tmp3.freq = tmp1->freq + tmp2->freq;
+		tmp3.left = tmp1;
+		tmp3.right = tmp2;
 		pushHeap(tmp3, &tree_heap, &heap_size);
 		order_num = nextOrderNumber(order_num);
 	}
 	*tree_heap_addr = tree_heap;
 }
 
+void removeTree(struct HuffNode_T* root) {
+	if (root->left != NULL) {
+		removeTree(root->left);
+	}
+	if (root->right != NULL) {
+		removeTree(root->right);
+	}
+	delete root;
+}
+
 /*------------- CODE END: Define a node in Huffman Tree -------------*/
 void LAPSE(string name) {
 	name = applyCaesarCipher(name);
 	cout << "New name: " << name << endl;
-	struct HuffNode_T** heap_for_huffman = NULL;	// Remind for cleaning up heap after finishing LAPSE
+	struct HuffNode_T* heap_for_huffman = NULL;	// Remind for cleaning up heap after finishing LAPSE
 	int heap_size = countFreqOfLetter(name, &heap_for_huffman);
 	buildHeap(&heap_for_huffman, heap_size);
 	for (int i = 0; i < heap_size; i++) {
-		cout << heap_for_huffman[i]->letter << " - " << heap_for_huffman[i]->freq << endl;
+		cout << heap_for_huffman[i].letter << " - " << heap_for_huffman[i].freq << endl;
 	}
 	buildHuff(&heap_for_huffman, heap_size);
+	struct HuffNode_T tmp[17];
+	for (int i = 0; i < 17; i++) {
+		tmp[i] = heap_for_huffman[i];
+	}
+	removeTree(&heap_for_huffman[0]);
+	for (int i = 0; i < 17; i++) {
+		tmp[i] = heap_for_huffman[i];
+	}
 }
 
 void KOKUSEN() {
